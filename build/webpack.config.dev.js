f7c219a58a6f31eae7cc8751feb096a98941c245
Piotr Olejniczak <piotr.olejniczak@makimo.pl>
2019-10-07 14:40:27 +0200

    Advanced setup, Jest tests are not working yet

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,19 +1,71 @@
 'use strict'
+
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+function resolve (dir) {
+    return path.join(__dirname, '..', dir)
+}
+
 module.exports = {
   mode: 'development',
-  entry: [
-    './src/app.js'
-  ],
-  module: {
+
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+
+    module: {
     rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      }
+        {
+          test: /\.(js|vue)$/,
+          use: 'eslint-loader',
+          enforce: 'pre'
+        },
+        {
+            test: /\.vue$/,
+            use: 'vue-loader'
+        },
+        {
+            test: /\.js$/,
+            use: 'babel-loader'
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader'
+            ]
+        },
+        {
+          test: /\.styl(us)?$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'stylus-loader'
+          ]
+        }
     ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
+        new CopyWebpackPlugin([{
+            from: resolve('static/img'),
+            to: resolve('dist/static/img'),
+            toType: 'dir'
+        }])
+    ]
 }
