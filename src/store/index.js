fa03fde4e4852b5baecc0de600e734b2e8c67a50
Piotr Olejniczak <piotr.olejniczak@makimo.pl>
2019-10-10 13:25:40 +0200

    Introduce Vuex store for Classic GoL and Classic GoL Tricolor

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import classicGoL from './modules/classicGoL';
+import GoLTricolor from './modules/GoLTricolor';
+// import stage from './modules/stage';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store(
+    {
+        modules: {
+            classicGoL,
+            GoLTricolor,
+            // game,
+        },
+    },
+);
